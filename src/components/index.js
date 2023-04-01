1b75793fce8a0d9efa8e3bc5e3321eb5c45c4523
devendra-dantal04 <devendra.dantal04@gmail.com>
2023-04-01 18:07:32 +0530

    Initial Commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import ColourPicker from "./ColourPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+
+export { CustomButton, AIPicker, ColourPicker, FilePicker, Tab }
